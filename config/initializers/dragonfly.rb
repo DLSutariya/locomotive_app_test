5e89a99d3637e9e39e2d9d6dae1c1f09ac9f3b7e
DilipSutariya <1992dilip@gmail.com>
2015-06-09 23:14:42 +0530

    Server installation

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,24 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret '5bd88c62c6b9f353473f272dac440693a99281b95f9ba4547304c2105ae3f9fd'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcmeCms::Application.config.secret_token = '117b0221c28d77ddf1ac01e3518e561936a8d9224892f0061289e5e1bb88d6e05744c6ba89a496ec5d5d5c5ef755bc12f1963a86ee533f1fcadd6dba7b8e71b2'
